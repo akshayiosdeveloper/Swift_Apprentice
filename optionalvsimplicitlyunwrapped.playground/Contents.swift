4edcfc7629489f279f2d47b28ef6dc3f57b9a1f5
akshayiosdeveloper <akshaychaudhary55@gmail.com>
2022-07-19 12:40:00 +0530

    example of optional vs implicitly wrapped

---
+++ optionalvsimplicitlyunwrapped.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var str: String      // normal 
+var str1:String! = "akshay"   // implicityUnwrapped optional
+var str2:String? = "kumar"  // optional
+// print(str)
+print(str1.lowercased())
+print(str2?.lowercased())
