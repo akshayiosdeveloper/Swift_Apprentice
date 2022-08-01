920ece90f28a2669aea4129303fbf814f7a8e589
akshayiosdeveloper <akshaychaudhary55@gmail.com>
2022-08-01 10:55:45 +0530

    access controls

---
+++ AccessControls/AccessControls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AccessControls
+//
+//  Created by Akshay Kumar on 19/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+ 
+}
+
+internal class Demo {
+    
+}
+
+open class TryDemo {
+    
+}
+
